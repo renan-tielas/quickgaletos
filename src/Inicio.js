c36165810909a097aeb5759dd75a01c6bfb0f94c
unknown <renantielas@poli.ufrj.br>
2020-06-30 09:02:18 -0300

    Site Galeto-mobile

--- src/Inicio.js
+++ src/Inicio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { isMobile } from 'react-device-detect';
 
-import InicioC from './InicioC'
+// import InicioC from './InicioC'
 import InicioN from './InicioN'
 
 
