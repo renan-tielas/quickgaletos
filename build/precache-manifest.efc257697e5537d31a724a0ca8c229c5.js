596f1a89bda4d473f4e44010f15ce3f985a52c6b
renan-tielas <renantielas@poli.ufrj.br>
2020-08-19 16:34:33 -0300

    com pasta build

---
+++ build/precache-manifest.efc257697e5537d31a724a0ca8c229c5.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58fd2a8c30829b8dc7e0e044e7d694d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f625b24f4f2cfc98f7bb",
+    "url": "/static/css/main.b86de7c3.chunk.css"
+  },
+  {
+    "revision": "17dadf165a36c05188bd",
+    "url": "/static/js/2.71ec145a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.71ec145a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f625b24f4f2cfc98f7bb",
+    "url": "/static/js/main.eab30f4a.chunk.js"
+  },
+  {
+    "revision": "960783107126786fa403",
+    "url": "/static/js/runtime-main.48ed64c8.js"
+  },
+  {
+    "revision": "2e6811c6e7e736bd3fd68c0499f97c51",
+    "url": "/static/media/arroz a grega.2e6811c6.JPG"
+  },
+  {
+    "revision": "92a2760254f7fb82ce94578f19c066bc",
+    "url": "/static/media/arroz com brocolis.92a27602.JPG"
+  },
+  {
+    "revision": "43f3675f08613805c41fd0fc8c5e7405",
+    "url": "/static/media/batata cozida.43f3675f.png"
+  },
+  {
+    "revision": "8e88dd398464c5bba610f51f41363d0d",
+    "url": "/static/media/batata frita.8e88dd39.jpg"
+  },
+  {
+    "revision": "1c024ffbae950a3609275ad694394210",
+    "url": "/static/media/cardapio-1.1c024ffb.jpg"
+  },
+  {
+    "revision": "159f5babef4c2ab8f35be1cfaec867e8",
+    "url": "/static/media/cardapio-2.159f5bab.jpg"
+  },
+  {
+    "revision": "4751113f6d4c2ce88d38b5d418020853",
+    "url": "/static/media/cardapio-3.4751113f.jpg"
+  },
+  {
+    "revision": "817a0e203330d8c9d3ca1e75362954d7",
+    "url": "/static/media/comunicado-1.817a0e20.png"
+  },
+  {
+    "revision": "7479e1ed2f98897f9ae8211026042ad8",
+    "url": "/static/media/espetinho pimentao e cebola.7479e1ed.png"
+  },
+  {
+    "revision": "e9e60beb3f9541667d08255f22618af5",
+    "url": "/static/media/espeto vegetariano.e9e60beb.JPG"
+  },
+  {
+    "revision": "dfaa9523a4aafa1cda292ca9cf83f30f",
+    "url": "/static/media/farofa brasileira.dfaa9523.jpg"
+  },
+  {
+    "revision": "1696b41d3b520f49057ecd813bd36c3b",
+    "url": "/static/media/farofa ovos.1696b41d.JPG"
+  },
+  {
+    "revision": "7131254cd4b333e4b4c4857e7970f438",
+    "url": "/static/media/feijao.7131254c.jpg"
+  },
+  {
+    "revision": "a943829555d05babbc00f2283ce21729",
+    "url": "/static/media/foto-1.a9438295.jpg"
+  },
+  {
+    "revision": "2f766b3a6b5b5c6db04c4e8338cfa600",
+    "url": "/static/media/foto-10.2f766b3a.jpg"
+  },
+  {
+    "revision": "fa0e63c512254f33b0de8eb9a6888915",
+    "url": "/static/media/foto-2.fa0e63c5.jpg"
+  },
+  {
+    "revision": "2c3dfdf362eeb7742fff767f5181fbc0",
+    "url": "/static/media/foto-4.2c3dfdf3.jpg"
+  },
+  {
+    "revision": "1f9ba2b2186ada003b9d253cd7df5f30",
+    "url": "/static/media/foto-5.1f9ba2b2.jpg"
+  },
+  {
+    "revision": "4f79164a4390507beb9d07bcbc4eec08",
+    "url": "/static/media/foto-6.4f79164a.jpg"
+  },
+  {
+    "revision": "c2fe2d3fbe96f89a6b7584ee721e20a0",
+    "url": "/static/media/foto-9.c2fe2d3f.jpg"
+  },
+  {
+    "revision": "a727c0eb7f86d9cac59f97376608089c",
+    "url": "/static/media/galeto.a727c0eb.jpg"
+  },
+  {
+    "revision": "e0e6ae71f99769c2c75896af6058b03b",
+    "url": "/static/media/legumes cozidos.e0e6ae71.JPG"
+  },
+  {
+    "revision": "6e6ff1e5574179b367a8f6228d0cd72b",
+    "url": "/static/media/linguiça.6e6ff1e5.jpg"
+  },
+  {
+    "revision": "9f52850ed91f5a78054bdcac15cbe454",
+    "url": "/static/media/logo.9f52850e.png"
+  },
+  {
+    "revision": "922c7ef26ed60d81405d6875391a24be",
+    "url": "/static/media/menu.922c7ef2.jpg"
+  },
+  {
+    "revision": "4bd9efb1eea1e01f283a9b351528e6fe",
+    "url": "/static/media/pudim.4bd9efb1.jpg"
+  },
+  {
+    "revision": "2ef16b5b97e2f3532834737de015f0d3",
+    "url": "/static/media/whatsapp-logo.2ef16b5b.png"
+  },
+  {
+    "revision": "fdca40e259db43e960206cacf115af0e",
+    "url": "/static/media/whatsapp-logo2.fdca40e2.jpg"
+  }
+]);
