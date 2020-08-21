4e656926b57b69a20d6ee781d2ec1d22dda76f55
unknown <renantielas@poli.ufrj.br>
2020-08-21 03:08:40 -0300

    progresso pedido 2108

--- src/Pedido/CategoriaPedido.js
+++ src/Pedido/CategoriaPedido.js
@@ -1,19 +1,26 @@
 import React from "react";
 import ItemPedido from './ItemPedido'
 import estilo from "./pedido-estilo.module.css";
-const CategoriaPedido = (props) => {
 
+
+const CategoriaPedido = (props) => {
+  
   return (
         
     <div className={estilo.categoria_comida_pedido}>
-          <ItemPedido texto={props.comidas[0]}/>
+{props.comidas.map((comida, i) => <ItemPedido texto={comida} key={i} />)}
+  
+        
+
+{/*           '
           <ItemPedido texto={props.comidas[1]}/>
           <ItemPedido texto={props.comidas[2]}/>
           <ItemPedido texto={props.comidas[3]}/>
-          <ItemPedido texto={props.comidas[4]}/>
+          <ItemPedido texto={props.comidas[4]}/> */}
     </div>
 
   );
-};
+
+}
 
 export default CategoriaPedido;
--- src/Pedido/Pedido.js
+++ src/Pedido/Pedido.js
@@ -10,29 +10,29 @@ const Pedido = (props) => {
   return (
 
   
-    <div className="div_Inicio">
-      <div className="div_topo">
+    <div className={estilo.div_Inicio}>
+      <div className={estilo.div_topo}>
         <Cabecalho />
         <Navegacao />
       </div>
 
 
-      <div className={estilo.main_div}>
+      <div className={estilo.main_div_pedido}>
 
 
       <CategoriaPedido titulo="Mais Pedidos" numero_ped="5" comidas={["galeto", "espetinho vegetariano","churrasqueto","salada","farofa de ovos"]}>
       </CategoriaPedido>
-      <CategoriaPedido titulo="Mais Pedidos" numero_ped="5" comidas={["galeto", "espetinho vegetariano","churrasqueto","salada","farofa de ovos"]}>
+      <CategoriaPedido titulo="Mais Pedidos" numero_ped="4" comidas={["galeto", "espetinho vegetariano","churrasqueto"]}>
       </CategoriaPedido>
-      <CategoriaPedido titulo="Mais Pedidos" numero_ped="5" comidas={["galeto", "espetinho vegetariano","churrasqueto","salada","farofa de ovos"]}>
+      <CategoriaPedido titulo="Mais Pedidos" numero_ped="3" comidas={["galeto", "espetinho vegetariano","salada","farofa de ovos"]}>
       </CategoriaPedido>
-      <CategoriaPedido titulo="Mais Pedidos" numero_ped="5" comidas={["galeto", "espetinho vegetariano","churrasqueto","salada","farofa de ovos"]}>
+      <CategoriaPedido titulo="Mais Pedidos" numero_ped="8" comidas={["galeto","galeto","galeto", "espetinho vegetariano","churrasqueto","salada","farofa de ovos"]}>
       </CategoriaPedido>
 
 
     </div>
 
-      <div className="div_footer">
+      <div className={estilo.div_footer}>
         <Footer />
       </div>
 
