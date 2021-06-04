152db3351488dc823c0754b618e4af115c7db932
unknown <renantielas@poli.ufrj.br>
2021-06-04 19:08:47 -0300

    primeiro commit

---
+++ pages/layout/Cabecalho.js
@@ -0,0 +1,13 @@
+import styles from '../../styles/cabecalho.module.css'
+
+
+const Cabecalho = () => {
+    return (
+      <div className={styles.cabecalho}>
+       
+      </div>
+    );
+  };
+  
+  export default Cabecalho;
+  
